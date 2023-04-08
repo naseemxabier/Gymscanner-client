df2917eb244c3c46b8524cb80294d6672cd9cab3
Xabier Naseem Mohammad <naseemxabier@gmail.com>
2023-04-08 17:42:09 +0200

    homepage creada

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer>
+   <p>Aquí va el footer</p> 
+  </footer>
+  
+  );
+}
+
+export default Footer;
